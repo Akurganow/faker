9a39bdde89f14d25dc7b4481b85c2ce2553839db
Alexander Kurganov <a.kurganow@gmail.com>
2023-11-21 16:24:06 +0400

    feat: Add Bookmarks

---
+++ __tests__/bookmarks.spec.ts
@@ -0,0 +1,27 @@
+// import { faker } from '@faker-js/faker'
+import Bookmarks from '../src/bookmarks'
+
+// https://developer.chrome.com/docs/extensions/reference/bookmarks/
+describe('faker/bookmarks', () => {
+	test('should be defined', () => {
+		const bookmarks = new Bookmarks()
+
+		expect(bookmarks).toBeDefined()
+		expect(bookmarks).toBeInstanceOf(Bookmarks)
+		expect(bookmarks).toHaveProperty('getBookmarks')
+		expect(bookmarks).toHaveProperty('getItem')
+		expect(bookmarks).toHaveProperty('reset')
+	})
+	test('if bookmark has children, each child should have a parentId equals to id of the bookmark', () => {
+		const bookmarks = new Bookmarks()
+		const items = bookmarks.getBookmarks()
+
+		for (const item of items) {
+			if (item.children) {
+				for (const child of item.children) {
+					expect(child.parentId).toBe(item.id)
+				}
+			}
+		}
+	})
+})
---
+++ src/bookmarks.ts
@@ -0,0 +1,75 @@
+import { faker } from '@faker-js/faker'
+import { BaseMock } from './types'
+import createBalancedArray from './utils/create-balanced-array'
+
+export interface MockBookmarksClass extends BaseMock {
+	getBookmarks(): MockBookmarksItem[]
+	getItem(): MockBookmarksItem
+}
+
+export interface MockBookmarksItem extends chrome.bookmarks.BookmarkTreeNode {
+}
+
+export interface MockBookmarksQuery extends chrome.bookmarks.BookmarkSearchQuery {
+	limit?: number
+}
+
+const DEFAULT_LIMIT = 100
+
+export default class MockBookmarks implements MockBookmarksClass {
+	public readonly query: MockBookmarksQuery
+	private bookmarks: MockBookmarksItem[] = []
+
+	constructor(query?: MockBookmarksQuery) {
+		this.query = query ?? {}
+		this.bookmarks = this.createMockBookmarks()
+	}
+
+	private createMockBookmarks(): MockBookmarksItem[] {
+		const limit = this.query.limit || DEFAULT_LIMIT
+		const rootItemsCount = faker.number.int({ min: 1, max: limit })
+		const childrenItemsCount = limit - rootItemsCount
+		const minCount = Math.min(rootItemsCount, childrenItemsCount)
+		const rootItemsChildrenCount = faker.helpers.shuffle(createBalancedArray(minCount, childrenItemsCount))
+
+		return faker.helpers.multiple(
+			() => this.createMockBookmarksItem(),
+			{ count: rootItemsCount }
+		).map((item, index) => {
+			return {
+				...item,
+				children: item.children && rootItemsChildrenCount[index] ? faker.helpers.multiple(
+					() => this.createMockBookmarksItem(item.id),
+					{ count: rootItemsChildrenCount[index] }
+				).map((child, index) => ({
+					...child,
+					index,
+				})) : undefined,
+			}
+		})
+	}
+
+	private createMockBookmarksItem(parentId?: string): MockBookmarksItem {
+		return {
+			id: faker.string.uuid(),
+			parentId,
+			url: this.query.url || faker.internet.url(),
+			title: this.query.title || faker.lorem.words(),
+			dateAdded: faker.date.recent().getTime(),
+			dateGroupModified: faker.date.recent().getTime(),
+			children: parentId ? undefined : [],
+		}
+	}
+
+	public getBookmarks(): MockBookmarksItem[] {
+		return this.bookmarks
+	}
+
+	public getItem(): MockBookmarksItem {
+		return faker.helpers.arrayElement(this.bookmarks)
+	}
+
+	public reset(): void {
+		this.bookmarks = this.createMockBookmarks()
+	}
+}
